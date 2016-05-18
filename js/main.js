c55d10cd473cac33c0da841548a929645ccb1bfa
sjtudesigner <zlp@sjtu.edu.cn>
2016-05-18 10:42:34 +0800

    HSL color

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ var gender = -1;
 var style = -1;
 var color = 0;
 var color_backup = 0;
-var color_set = -1;
 var attrs_backup = new Array(10);
 var attrs = new Array(10);
 var img_addrs = [];
@@ -48,10 +47,14 @@ function pack_info(type, info_obj)
             var area_replace_str = info_obj.img_replace_area.join(",");
             var sub_area_replace_str = info_obj.sub_replace_area.join(",");
             var attr_str = info_obj.attr.join(";");
+            var m_color = -1;
+            if (info_obj.color != -1) {
+                m_color = info_obj.color[0].toFixed(2) + ";" + info_obj.color[1].toFixed(2) + ";" + info_obj.color[2].toFixed(2);
+            }
             info_str = type + ";" + info_obj.gender + ";" + 
                 info_obj.style + ";" + area_str + ";" + 
                 info_obj.imgurl + ";" + alo + ";" + nu + ";" + attr_str + 
-                ";" + info_obj.hue + ";" + info_obj.subPlaneUrl + ";" + 
+                ";" + m_color + ";" + info_obj.subPlaneUrl + ";" + 
                 area_replace_str + ";" + sub_area_replace_str + ";";
             break;
         case 2:
@@ -118,7 +121,7 @@ function submit(type, info_obj, callback) {
 
 function callback_uploadImage(result) {
     // The server is not always on, thus I add this when testing.
-    // result = "<RESULTS><RESULT>13</RESULT><RESULT>26</RESULT><RESULT>100</RESULT><RESULT>200</RESULT></RESULTS>"
+    result = "<RESULTS><RESULT>13</RESULT><RESULT>26</RESULT><RESULT>100</RESULT><RESULT>200</RESULT></RESULTS>"
     var region_res = [];
     $(result).find('RESULT').each(function() {
         region_res.push(parseInt($(this).text()));
@@ -136,22 +139,22 @@ function firstRetrieval() {
 }
 
 function callback_firstRetrieval(result) {
-    /* result = "<RESULTS><RESULT>256</RESULT><RESULT>10</RESULT><RESULT>1</RESULT><RESULT>2</RESULT> \
+    result = "<RESULTS><RESULT>0.7</RESULT><RESULT>0.5</RESULT><RESULT>0.3</RESULT><RESULT>10</RESULT><RESULT>1</RESULT><RESULT>2</RESULT> \
     <RESULT>3</RESULT><RESULT>4</RESULT><RESULT>1</RESULT><RESULT>2</RESULT> \
     <RESULT>3</RESULT><RESULT>4</RESULT><RESULT>1</RESULT><RESULT>2</RESULT><RESULT>img/1.jpg</RESULT><RESULT>200</RESULT><RESULT>100</RESULT></RESULTS>";
-    */
     var tmp = [];
     $(result).find('RESULT').each(function() {
         tmp.push($(this).text());
     });
-    if (tmp.length < 2 || tmp.length < 2 + parseInt(tmp[1])) alert("FATAL ERROR");
-    color = color_backup = color_set = parseInt(tmp[0]);
-    for (var i = 2;i < parseInt(tmp[1]) + 2;i++)
+    if (tmp.length < 4 || tmp.length < 4 + parseInt(tmp[3])) alert("FATAL ERROR");
+    color = [parseFloat(tmp[0]), parseFloat(tmp[1]), parseFloat(tmp[2])];
+    color_backup = [parseFloat(tmp[0]), parseFloat(tmp[1]), parseFloat(tmp[2])];
+    for (var i = 4;i < parseInt(tmp[3]) + 4;i++)
     {
-        attrs[i - 2] = attrs_backup[i - 2] = parseInt(tmp[i]);
+        attrs[i - 4] = attrs_backup[i - 4] = parseInt(tmp[i]);
     }
     img_addrs = [];
-    for (var i = 2 + parseInt(tmp[1]);i < tmp.length;i += 3)
+    for (var i = 4 + parseInt(tmp[3]);i < tmp.length;i += 3)
     {
         // TODO: result here
         img_addrs.push([tmp[i], tmp[i+1], tmp[i+2]]);
@@ -213,11 +216,11 @@ function firstRetrieval_setAttrs() {
     page_init();
     toggle_page(1);
 }
-
+/*
 function rgb2h(r, g, b) {
     max = Math.max(r, g, b);
     min = Math.min(r, g, b);
-    var h = 0;
+    var h = 0.0, s = 0.0, v = 0.0;
     if (max == min) h = 0;
     else if (max == r && g >= b) {
         h = Math.floor(60 * (g - b) / (max - min));
@@ -231,31 +234,27 @@ function rgb2h(r, g, b) {
     else {
         h = Math.floor(60 * (r - g) / (max - min) + 240);
     }
-    return h;
-}
-
-function h2rgb(h) {
-    var hi = Math.floor(h / 60) % 6;
-    var f = h / 60 - hi;
-    var p = 0;
-    var q = parseInt((1 - f) * 255);
-    var t = parseInt(f * 255);
-    var v = 255;
-    
-    switch (hi) {
-        case 0:
-            return [v, t, p];
-        case 1:
-            return [q, v, p];
-        case 2:
-            return [p, v, t];
-        case 3:
-            return [p, q, v];
-        case 4:
-            return [t, p, v];
-        case 5:
-            return [v, p, q];
+    v = max / 255;
+    if (max != 0) {
+        s = 1 - min / max;
     }
+    return [h / 360, s, v];
+}
+*/
+function h2rgb(color) {
+    var h = color[0], s = color[1], l = color[2];
+    var q = (l < 0.5) ? (l * (1 + s)) : (l + s - l * s);
+    var p = 2 * l - q;
+    t = [h + 1 / 3, h, h - 1 / 3]
+    for (i in t) {
+        t[i] = (t[i] > 1) ? t[i] - 1 : ((t[i] < 0) ? t[i] + 1 : t[i]);
+        if (t[i] < 1 / 6) t[i] = p + ((q - p) * 6 * t[i]);
+        else if (t[i] < 1 / 2) t[i] = q;
+        else if (t[i] < 2 / 3) t[i] = p + (q - p) * 6 * (2 / 3 - t[i]);
+        else t[i] = p;
+        t[i] = parseInt(t[i] * 255);
+    }
+    return t;
 }
 
 function uploadSubPlane(file) {
@@ -290,13 +289,13 @@ function secondRetrieval() {
         k++;
     });
 	var info_obj = {imgurl: imgUrl, gender: gender, style: style, area_arr: selection_arr, attr: attrs,
-			hue: (color == color_backup) ? -1 : color, subPlaneUrl: imgSubPlane, 
+			color: (color == color_backup) ? -1 : color, subPlaneUrl: imgSubPlane, 
 			img_replace_area: attr_areas[0], sub_replace_area: attr_areas[1]};
 	submit(1, info_obj, callback_secondRetrieval);
 }
 
 function callback_secondRetrieval(result) {
-	// result = "<RESULTS><RESULT>img/2.jpg</RESULT><RESULT>200</RESULT><RESULT>100</RESULT></RESULTS>";
+	result = "<RESULTS><RESULT>img/2.jpg</RESULT><RESULT>200</RESULT><RESULT>100</RESULT></RESULTS>";
     var tmp = [];
     $(result).find('RESULT').each(function() {
         tmp.push($(this).text());
